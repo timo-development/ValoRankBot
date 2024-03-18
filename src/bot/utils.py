f8380246ba50ca3a91672d493377d1ab61733940
Timo <timoguelich@gmail.com>
2024-03-18 15:23:51 +0100

    update utils.py

--- src/bot/utils.py
+++ src/bot/utils.py
@@ -8,6 +8,7 @@ class BotConfig:
     '''dataclass for storing bot config data'''
     auth_channel_id: int
     bot_token: str
+    client_id: int
     client_secret: str
     support_server_id: int
 
