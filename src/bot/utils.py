dda8016b9fe383e14ff47fdb612ac42b5cafd456
Timo <timoguelich@gmail.com>
2024-03-19 10:36:06 +0100

    update utils.py: added comments

--- src/bot/utils.py
+++ src/bot/utils.py
@@ -6,12 +6,24 @@ from typing import Optional, List
 @dataclass
 class BotConfig:
     '''dataclass for storing bot config data'''
+
     auth_channel_id: int
+    '''the channel.id where the bot will reveive AuthEvents'''
+
     bot_token: str
+    '''Discord bot token'''
+
     client_id: int
+    '''the id of the app / bot.user.id'''
+
     client_secret: str
+    '''Discord client secret used for OAuth2'''
+
     support_server_id: int
+    '''the guild.id of the bots support server'''
+
     valo_api_key: Optional[str]
+    '''Optional: api key for https://github.com/Henrik-3/unofficial-valorant-api'''
 
 
 @dataclass
